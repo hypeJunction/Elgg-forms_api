01ce5c3064567e28db321d657ded0358c5dc223e
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-03-23 13:35:19 +0100

    fix(requirements): do not restrict Elgg version

--- activate.php
+++ activate.php
@@ -1,6 +1,2 @@
 <?php
 
-if (version_compare(elgg_get_version(true), '2.1', '>=')) {
-	register_error('Forms API plugin implements features that have already been integrated in core.');
-	return false;
-}
