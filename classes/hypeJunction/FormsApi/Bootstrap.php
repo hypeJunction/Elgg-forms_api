7cf1a9990d1d0e84cec0811c9229096e578a16e2
ismayil.khayredinov@holoplot.com <ismayil.khayredinov@holoplot.com>
2026-04-15 16:06:39 +0200

    migrate(4.x): automated AST transformations + docblock additions

---
+++ classes/hypeJunction/FormsApi/Bootstrap.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace hypeJunction\FormsApi;
+
+use Elgg\PluginBootstrap;
+
+class Bootstrap extends PluginBootstrap {
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function load() {}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function boot() {}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function init() {
+		elgg_extend_view('css/elgg', 'elements/forms/field.css');
+		elgg_extend_view('css/admin', 'elements/forms/field.css');
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function ready() {}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function shutdown() {}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function activate() {}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function deactivate() {}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function upgrade() {}
+}
---
+++ elgg-plugin.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'plugin' => [
+		'name' => 'Forms API',
+		'version' => '4.0.0',
+	],
+	'bootstrap' => \hypeJunction\FormsApi\Bootstrap::class,
+];
